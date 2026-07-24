428f6404391dbfcf5bcb096459ad29e1c322118c
Srimanta Nayak <ssrimanta2006@gmail.com>
2026-07-24 19:53:50 +0530

    Time: 20 ms (62.59%), Space: 79.2 MB (35.94%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,12 +3,5 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    nums.sort((a,b) => a-b);
-    const set = [...new Set(nums)];
-    if(set.length < nums.length){
-        return true; 
-    }
-    else{
-        return false ; 
-    }
+    return new Set(nums).size !== nums.length;
 };
